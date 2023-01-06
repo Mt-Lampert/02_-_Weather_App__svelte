1e9dc65673e40d9398cedca7251ef243404d62d4
Mt-Lampert <perrin.blog@web.de>
2023-01-06 18:48:23 +0100

    p: Refactoring: outsourcing center into <Center>

---
+++ src/lib/center.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { getSVGfor } from './helpers';
+
+	export let place: string = 'London';
+	export let temperature: string = '10';
+	export let sky: string = 'cloudy';
+
+	$: skyImage = 'images/' + getSVGfor(sky);
+</script>
+
+<div class="center">
+	<p>
+		<img src={skyImage} alt="Sky Clipart" />
+	</p>
+	<h1 class="title">{place}</h1>
+	<h2 class="subtitle">{temperature}</h2>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Center from '$lib/center.svelte';
 	import Footer from '$lib/footer.svelte';
 	import type { CompState } from './myTypes';
 
@@ -20,19 +21,23 @@
 </script>
 
 <section class="top section is-flex">
-	<div class="field">
-		<div class="control">
-			<input class="input" name="place" type="text" placeholder="The place" bind:value={place} />
+	<form on:submit|preventDefault={lookup}>
+		<div class="field">
+			<div class="control">
+				<input class="input" name="place" type="text" placeholder="The place" bind:value={place} />
+			</div>
 		</div>
-	</div>
-	<div><button class="button" on:click={lookup}>Lookup</button></div>
+		<div><button class="button">Lookup</button></div>
+	</form>
 </section>
 
 <section class="section center">
 	{#if myState.state === 'success'}
-		<h1 class="title">{myState.payload.place}</h1>
-		<h2 class="subtitle">{myState.payload.temperature}</h2>
-		<p><b>Sky:</b>{myState.payload.sky}</p>
+		<Center
+			place={myState.payload.place}
+			temperature={myState.payload.temperature}
+			sky={myState.payload.sky}
+		/>
 	{:else if myState.state === 'fail'}
 		<div class="notification is-danger" transition:fly={{ x: 100, duration: 1500 }}>
 			{myState.error}
