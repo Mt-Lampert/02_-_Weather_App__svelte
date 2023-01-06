1b5b11c7c7da901d7a9c7fcc9a8ee1c77e822ee0
Mt-Lampert <perrin.blog@web.de>
2023-01-06 06:08:29 +0100

    p: going TypeScript

---
+++ src/lib/helpers.ts
@@ -0,0 +1,30 @@
+/** 
+  * returns an SVG clipart file name depending on weather conditions
+  * 
+  * @param desc description for the weather condition
+  * @returns the name of the clipart file
+  */
+export function getSVGfor(desc: string): string {
+  let mySVG = "";
+  switch (desc) {
+    case "cloudy":
+      mySVG = "cloudy.svg";
+      break;
+    case "few clouds":
+    case "sunny":
+      mySVG = "sunny.svg";
+      break;
+    case "light snow":
+      mySVG = "light_snow.svg";
+      break;
+    case "overcast":
+      mySVG = "overcast.svg";
+      break;
+    case "rain":
+      mySVG = "rain.svg";
+      break;
+    default:
+      return "EH?";
+  }
+  return mySVG;
+}
