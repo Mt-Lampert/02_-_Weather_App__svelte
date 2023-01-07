1b876ee0db91ac63326f6e4a4a30939b7bb4e826
Mt-Lampert <perrin.blog@web.de>
2023-01-07 09:06:32 +0100

    correcting errors

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,19 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+  plugins: [sveltekit()],
 
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  },
 
-	css: {
-		preprocessorOptions: {
-			scss: {
-				additionalData: '@use "src/variables.scss" as *;'
-			}
-		}
-	}
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "src/variables.scss" as *;'
+      }
+    }
+  }
 };
 
 export default config;
